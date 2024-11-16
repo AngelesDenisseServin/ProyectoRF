64d7cb384d7bb64ef318101c78c02d9b460b33e7
AngelesDenisseServin <123780960+AngelesDenisseServin@users.noreply.github.com>
2024-11-16 14:36:15 -0700

    frames.py

--- frames.py
+++ frames.py
@@ -3,23 +3,25 @@ from deepface import DeepFace
 import time
 import os  
 
-video = cv2.VideoCapture('videograbado/video5.mp4')
-cara_persona_extraviada = cv2.imread('Galeria/Denisse/cara_t.png')
-carpeta_guardado = 'Fotogramas_video5'
+video = cv2.VideoCapture('videos/video1.mp4')
+cara_persona_extraviada = cv2.imread('Galeria/Denisse/cara.png') 
+carpeta_guardado = 'Fotogramas_video1_videos'
+inicio = time.time()
 if not os.path.exists(carpeta_guardado):
     os.makedirs(carpeta_guardado)
-def verificar_cara(cara_recortada, cara_extraviada):
-    resultado_cara = DeepFace.verify(cara_recortada, cara_extraviada, 
+def verificar_cara(Fotograma, cara_extraviada):
+    resultado = DeepFace.verify(Fotograma, cara_extraviada, 
                                 model_name="Facenet512", 
                                 distance_metric="cosine", 
                                 enforce_detection=False,  
                                 detector_backend="mtcnn", 
-                                threshold=0.3, 
+                                threshold=0.4, 
                                 normalization="base")
-    return resultado_cara['verified']
+   
+    return resultado['verified'], resultado
 
 contador_fotogramas = 0
-procesar_cada_n_fotogramas =20   
+#procesar_cada_n_fotogramas =5   
 
 if not video.isOpened():
     print("Error al abrir el archivo de video.")
@@ -37,35 +39,35 @@ while video.isOpened():
     
     contador_fotogramas += 1
 
-    if contador_fotogramas % procesar_cada_n_fotogramas == 0:
-        try:
-            fotograma=cv2.resize(fotograma,(0,0),fx=0.7,fy=0.7)
-            caras = cascada_cara.detectMultiScale(fotograma, 1.1, 5, minSize=(24, 24))
-            for (x, y, w, h) in caras:
-                caradeimagen_recortada = fotograma[y:y+h, x:x+w]  
-                hay_cara = verificar_cara(caradeimagen_recortada, cara_persona_extraviada)
-               
-                if hay_cara:
-                    cv2.rectangle(fotograma, (x, y), (x+w, y+h), (0, 255, 0), 2)
-                    cv2.putText(fotograma, 'Coincidencia Encontrada!', (x-10, y-10), 
-                                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-                    cv2.imshow('Coincidencia', fotograma)
-
-                    ruta_guardado = os.path.join(carpeta_guardado, f'fotograma_{contador_fotogramas}.png')
-                    cv2.imwrite(ruta_guardado, fotograma)
-
+    #if contador_fotogramas % procesar_cada_n_fotogramas == 0:
+    try: 
+            ver, resultado_verificacion = verificar_cara(fotograma, 'Galeria/Denisse/carap.png')
+            if ver:
+                datos_cara = resultado_verificacion['facial_areas']['img1']
+                x = datos_cara['x']
+                y = datos_cara['y']
+                w = datos_cara['w']
+                h = datos_cara['h']
+                cv2.rectangle(fotograma, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                cv2.putText(fotograma, 'Coincidencia Encontrada!', (x-10, y-10), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+                cv2.imshow('Coincidencia', fotograma)
+               # nombre_imagen = os.path.join(carpeta_guardado, f'coincidencia_{contador_fotogramas}.png')
+                #cv2.imwrite(nombre_imagen, fotograma)
+                #print(f'Imagen guardada: {nombre_imagen}')
             else:
-                cv2.putText(fotograma, 'Sin Coincidencia', (50, 50), 
-                            cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
-                ruta_guardado = os.path.join(carpeta_guardado, f'fotograma_{contador_fotogramas}_1.png')
-                cv2.imwrite(ruta_guardado, fotograma)
-        except Exception as e:
+                print("Sin coincidencia")
+ 
+    
+            fin = time.time()
+            print("Tiempo del programa es:", (fin - inicio) * 10**3, "ms")
+            nombre_imagen = os.path.join(carpeta_guardado, f'coincidencia_{contador_fotogramas}.png')
+            cv2.imwrite(nombre_imagen, fotograma)
+            print(f'Imagen guardada: {nombre_imagen}')
+    except Exception as e:
             print(f"Error al verificar rostro: {e}")
 
     cv2.imshow('Video', fotograma)
-    
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
 
 fin = time.time()
 print("Tiempo del programa es:", (fin - inicio) * 10**3, "ms")
